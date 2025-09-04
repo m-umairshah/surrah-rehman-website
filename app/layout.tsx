59843a5e6837ac3ea8df5f5a51645df4602605e2
Muhammad Umair <wordpresspeerumair@gmail.com>
2025-09-04 12:54:36 +0500

    search not found removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -197,6 +197,16 @@ export default function RootLayout({
           }}
         />
 
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "t5e81z8weg");
+          `}
+        </Script>
+
         {/* Google Analytics (gtag) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-XYE936JLWC"
